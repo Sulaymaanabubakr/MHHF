65b0819b55f700c28c7d41a45747158cfb1fa876
Sulaymaan Abubakr <110928751+Sulaymaanabubakr@users.noreply.github.com>
2025-10-21 09:52:13 +0100

    Add Firebase-powered media admin and dynamic galleries

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,14 @@
+export const firebaseConfig = {
+  apiKey: 'YOUR_FIREBASE_API_KEY',
+  authDomain: 'YOUR_FIREBASE_AUTH_DOMAIN',
+  projectId: 'YOUR_FIREBASE_PROJECT_ID',
+  storageBucket: 'YOUR_FIREBASE_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_FIREBASE_MESSAGING_SENDER_ID',
+  appId: 'YOUR_FIREBASE_APP_ID',
+};
+
+export const cloudinaryConfig = {
+  cloudName: 'your_cloud_name',
+  uploadPresetImages: 'mhhf_image_unsigned_preset',
+  uploadPresetVideos: 'mhhf_video_unsigned_preset',
+};
