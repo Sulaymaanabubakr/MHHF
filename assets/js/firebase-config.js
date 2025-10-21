720e6911bcbbd7dc77d83a82462b461ccc1098a6
Sulaymaan Abubakr <abubakrysulaimaan24434@gmail.com>
2025-10-21 16:02:47 +0100

    updated firebase and cloudinary

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,14 +1,15 @@
 export const firebaseConfig = {
-  apiKey: 'YOUR_FIREBASE_API_KEY',
-  authDomain: 'YOUR_FIREBASE_AUTH_DOMAIN',
-  projectId: 'YOUR_FIREBASE_PROJECT_ID',
-  storageBucket: 'YOUR_FIREBASE_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_FIREBASE_MESSAGING_SENDER_ID',
-  appId: 'YOUR_FIREBASE_APP_ID',
+  apiKey: "AIzaSyCaV-9AbMtV56bluiAgjkleaStvh8jeAE8",
+  authDomain: "mhhf-e64bd.firebaseapp.com",
+  projectId: "mhhf-e64bd",
+  storageBucket: "mhhf-e64bd.firebasestorage.app",
+  messagingSenderId: "294712995871",
+  appId: "1:294712995871:web:6f74431c0fe2005a08652a",
+  measurementId: "G-SK38VPDN6B"
 };
 
 export const cloudinaryConfig = {
-  cloudName: 'your_cloud_name',
-  uploadPresetImages: 'mhhf_image_unsigned_preset',
-  uploadPresetVideos: 'mhhf_video_unsigned_preset',
+  cloudName: 'dcwxl9d9t',
+  uploadPresetImages: 'mhhf_image_upload',
+  uploadPresetVideos: 'mhhf_video_upload',
 };
